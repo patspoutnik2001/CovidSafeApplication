a635c447f2e9009c1c6b490e9514550564913872
Patryk Krasucki <krasuckipatryk2001@gmail.com>
2022-10-01 21:42:15 +0200

    add creation compte

--- app/src/main/java/com/example/covidsafeapplication/LoginActivity.java
+++ app/src/main/java/com/example/covidsafeapplication/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.covidsafeapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,12 +10,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button loginBtn;
+    Button loginBtn,registerBtn;
     TextView email_input,pass_input;
     private FirebaseAuth mAuth;
 
@@ -24,16 +29,46 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login2);
 
         loginBtn = (Button) findViewById(R.id.btn_login);
+        registerBtn = (Button) findViewById(R.id.btn_register);
         email_input = (TextView) findViewById(R.id.email_input);
         pass_input = (TextView) findViewById(R.id.password_input);
         mAuth = FirebaseAuth.getInstance();
 
+        //TODO: verif bug
+        if (mAuth.getCurrentUser() != null){
+            //startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            //finish();
+        }
 
-
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 loginUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        String email = email_input.getText().toString().trim();
+        String pass = pass_input.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "User Created", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                }else{
+                    Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
     }
