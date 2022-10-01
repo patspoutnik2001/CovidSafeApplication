0a9b3409387c074bdc6e115924621b154d48e116
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-10-01 19:38:11 +0200

    Ajout photo android (non fonctionnel)

--- app/src/main/java/com/example/covidsafeapplication/PhotoActivity.java
+++ app/src/main/java/com/example/covidsafeapplication/PhotoActivity.java
@@ -1,36 +1,75 @@
 package com.example.covidsafeapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.graphics.Camera;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PhotoActivity extends AppCompatActivity {
+
+    TextView testView;
+
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+
+    private final String tag = "VideoServer";
+
+    Button start, stop;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
 
-        Button loginBtn = (Button) findViewById(R.id.login);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                goToLocal();
+//        Button loginBtn = (Button) findViewById(R.id.login);
+//        loginBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                goToLocal();
+//            }
+//        });
+
+        start = (Button)findViewById(R.id.qrCodePhoto);
+        start.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View arg0) {
+                start_camera();
             }
         });
 
+//        stop = (Button)findViewById(R.id.btn_stop);
+//        stop.setOnClickListener(new Button.OnClickListener() {
+//            public void onClick(View arg0) {
+//                stop_camera();
+//            }
+//        });
 
-
+//        surfaceView = (SurfaceView)findViewById(R.id.surfaceView1);
+//        surfaceHolder = surfaceView.getHolder();
+//        surfaceHolder.addCallback(this);
+//        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
     }
+
 
     private void goToLocal() {
         Intent intent = new Intent(this, LocalActivity.class);
         startActivity(intent);
     }
+
+    private void start_camera() {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
+    }
+
+
 
 
 }
