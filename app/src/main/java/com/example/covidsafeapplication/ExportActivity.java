bd23642846ea7f99636aa5bafa24b3ebc8d63649
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-10-02 23:03:26 +0200

    Export PDF (non fonctionnel)
    Export PDF (non fonctionnel)

---
+++ app/src/main/java/com/example/covidsafeapplication/ExportActivity.java
@@ -0,0 +1,172 @@
+package com.example.covidsafeapplication;
+
+
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.fonts.Font;
+import android.graphics.pdf.PdfDocument;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import org.w3c.dom.Document;
+
+public class ExportActivity extends AppCompatActivity {
+
+
+    // variables for our buttons.
+    Button generatePDFbtn;
+
+    // declaring width and height
+    // for our PDF file.
+    int pageHeight = 1120;
+    int pagewidth = 792;
+
+    // creating a bitmap variable
+    // for storing our images
+
+
+    // constant code for runtime permissions
+    private static final int PERMISSION_REQUEST_CODE = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export);
+
+        // initializing our variables.
+        generatePDFbtn = findViewById(R.id.idBtnGeneratePDF);
+
+
+        // below code is used for
+        // checking our permissions.
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
+
+        generatePDFbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling method to
+                // generate our PDF file.
+                generatePDF();
+            }
+        });
+    }
+
+    private void generatePDF() {
+
+
+        // create a PDF file with A in it
+        PdfDocument document = new PdfDocument();
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(100, 100, 1).create();
+        PdfDocument.Page page = document.startPage(pageInfo);
+        Canvas canvas = page.getCanvas();
+        Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        canvas.drawText("A", 50, 50, paint);
+        document.finishPage(page);
+        // write the document content
+
+
+        File file = new File(Environment.getExternalStorageDirectory(), "GFG.pdf");
+
+        try {
+            // after creating a file name we will
+            // write our PDF file to that location.
+            document.writeTo(new FileOutputStream(file));
+
+            // below line is to print toast message
+            // on completion of PDF generation.
+            Toast.makeText(ExportActivity.this, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            // below line is used
+            // to handle error
+            e.printStackTrace();
+        }
+        // after storing our pdf to that
+        // location we are closing our PDF file.
+        document.close();
+
+//        String targetPdf = "a.pdf";
+//        File filePath = new File(targetPdf);
+
+
+
+
+
+//        try {
+//            File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES);
+//            File file = new File(path, "/" + "test.pdf");
+//            document.writeTo(new FileOutputStream(path));
+//            Toast.makeText(this, "Done", Toast.LENGTH_LONG).show();
+//
+//        } catch (IOException e) {
+//            Log.e("main", "error " + e.toString());
+//            Toast.makeText(this, "Something wrong: " + e.toString(),  Toast.LENGTH_LONG).show();
+//        }
+//        // close the document
+//        document.close();
+
+
+
+
+
+
+    }
+
+    private boolean checkPermission() {
+        // checking of permissions.
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        // requesting permissions if not provided.
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+
+                // after requesting permissions we are showing
+                // users a toast message of permission granted.
+                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if (writeStorage && readStorage) {
+                    Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denied.", Toast.LENGTH_SHORT).show();
+                    //finish();
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/covidsafeapplication/LocalActivity.java
+++ app/src/main/java/com/example/covidsafeapplication/LocalActivity.java
@@ -2,13 +2,34 @@ package com.example.covidsafeapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LocalActivity extends AppCompatActivity {
+
+    ImageButton exportPDF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local);
+
+        exportPDF = findViewById(R.id.exportPDF);
+
+        exportPDF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToExport();
+
+            }
+        });
+    }
+
+    private void goToExport() {
+        Intent intent = new Intent(this, ExportActivity.class);
+        startActivity(intent);
     }
 }
