021e7949e0a3b4ebda797df39eaae2e5964f3bbf
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-10-07 22:48:42 +0200

    PDF fonctionnel

--- app/src/main/java/com/example/covidsafeapplication/ExportActivity.java
+++ app/src/main/java/com/example/covidsafeapplication/ExportActivity.java
@@ -2,169 +2,82 @@ package com.example.covidsafeapplication;
 
 
 
-import android.content.ContextWrapper;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Typeface;
-import android.graphics.pdf.PdfDocument;
-import android.graphics.pdf.PdfRenderer;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-
-import android.util.Log;
-import android.util.PrintWriterPrinter;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.nio.ByteBuffer;
-
-
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
-
-import static com.google.android.material.internal.ViewUtils.getContentView;
-
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-
 
 public class ExportActivity extends AppCompatActivity {
 
+    private EditText editText;
+    private TextView textView;
 
-    // variables for our buttons.
-    Button generatePDFbtn;
-    private byte[] bytes = null;
-
-
-    // declaring width and height
-    // for our PDF file.
-    int pageHeight = 1120;
-    int pagewidth = 792;
-
-    // creating a bitmap variable
-    // for storing our images
-
-
-    // constant code for runtime permissions
-    private static final int PERMISSION_REQUEST_CODE = 200;
+    private String stringFilePath = Environment.getExternalStorageDirectory().getPath() + "/Download/ProgrammerWorld.pdf";
+    private File file = new File(stringFilePath);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
 
-        // initializing our variables.
-        generatePDFbtn = findViewById(R.id.idBtnGeneratePDF);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
 
-
-        // below code is used for
-        // checking our permissions.
-        if (checkPermission()) {
-            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-        } else {
-            requestPermission();
-        }
-
-        generatePDFbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // calling method to
-                // generate our PDF file.
-                try {
-                    generatePDF();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
     }
 
-    private void generatePDF() throws IOException {
+    public void buttonCreatePDF(View view){
 
-        System.out.println("ENTRERED GENERATE PDF");
-
-        // create a new document
-        PdfDocument document = new PdfDocument();
-        // crate a page description
+        PdfDocument pdfDocument = new PdfDocument();
         PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(300, 600, 1).create();
-        // start a page
-        PdfDocument.Page page = document.startPage(pageInfo);
-        Canvas canvas = page.getCanvas();
+        PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+
         Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        canvas.drawCircle(50, 50, 30, paint);
-        paint.setColor(Color.BLACK);
-        canvas.drawText("blabla", 80, 50, paint);
-        //canvas.drawt
-        // finish the page
-        document.finishPage(page);
-// draw text on the graphics object of the page
-        // Create Page 2
-        pageInfo = new PdfDocument.PageInfo.Builder(300, 600, 2).create();
-        page = document.startPage(pageInfo);
-        canvas = page.getCanvas();
-        paint = new Paint();
-        paint.setColor(Color.BLUE);
-        canvas.drawCircle(100, 100, 100, paint);
-        document.finishPage(page);
-        // write the document content
-        String directory_path = Environment.getExternalStorageDirectory().getPath() + "/mypdf/";
-        File file = new File(directory_path);
-        if (!file.exists()) {
-            file.mkdirs();
+        String stringPDF = editText.getText().toString();
+
+        int x = 10, y = 25;
+
+        for (String line:stringPDF.split("\n")){
+            page.getCanvas().drawText(line,x,y, paint);
+
+            y+=paint.descent()-paint.ascent();
         }
-        String targetPdf = directory_path+"test-2.pdf";
-        File filePath = new File(getFilePath());
+        pdfDocument.finishPage(page);
         try {
-            document.writeTo(new FileOutputStream(filePath));
-            Toast.makeText(this, "Done", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            Log.e("main", "error "+e.toString());
-            Toast.makeText(this, "Something wrong: " + e.toString(),  Toast.LENGTH_LONG).show();
+            pdfDocument.writeTo(new FileOutputStream(file));
         }
-        // close the document
-        document.close();
+        catch (Exception e){
+            e.printStackTrace();
+            textView.setText("Error in Creating");
+        }
+        pdfDocument.close();
     }
 
-
-
-    private boolean checkPermission() {
-        // checking of permissions.
-        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
-        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
-        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
-    }
-
-    private void requestPermission() {
-        // requesting permissions if not provided.
-        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
-    }
-
-
-    private String getFilePath(){
-        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
-        File directory = contextWrapper.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
-        File file = new File(directory, "MonPutinDePDF.pdf");
-
-        return file.getPath();
+    public void buttonReadPDF(View view){
+        try {
+            PdfReader pdfReader = new PdfReader(file.getPath());
+            String stringParse = PdfTextExtractor.getTextFromPage(pdfReader,1).trim();
+            pdfReader.close();
+            textView.setText(stringParse);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            textView.setText("Error in Reading");
+        }
     }
 }
