dab534164b48db2eb44e983a581c285047473644
~~ Christos ~~ <christos.angelopoulos@helb-prigogine.be>
2022-10-07 19:38:20 +0200

    Retour de latitude longitude fonctionnelle

--- app/src/main/java/com/example/covidsafeapplication/AddBatiment.java
+++ app/src/main/java/com/example/covidsafeapplication/AddBatiment.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +16,14 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+
 public class AddBatiment extends AppCompatActivity {
 
     Button btn_addBatiment;
     TextView nomBatiment_input,adresse_input;
     private FirebaseAuth mAuth;
+    Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +34,31 @@ public class AddBatiment extends AppCompatActivity {
         nomBatiment_input = (TextView) findViewById(R.id.nomBatiment_input);
         adresse_input = (TextView) findViewById(R.id.adresse_input);
 
+
+
         btn_addBatiment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addBatiment();
+                //addBatiment();
+                // TODO: remettre add batiment
+
+                testLocalisation();
             }
         });
     }
 
+    private void testLocalisation() {
+        geocoder = new Geocoder(this);
 
+        try {
+            geocoder.getFromLocationName(adresse_input.getText().toString(), 1);
+            System.out.println(geocoder.getFromLocationName(adresse_input.getText().toString(), 1));
+        } catch (IOException e) {
+            System.out.println("l'adresse n'existe pas !!!!!!!!!!!!");
+
+        }
+
+    }
 
 
     private void addBatiment() {
--- app/src/main/java/com/example/covidsafeapplication/Batiment.java
+++ app/src/main/java/com/example/covidsafeapplication/Batiment.java
@@ -5,25 +5,11 @@ import android.location.Geocoder;
 public class Batiment {
     int idBatiment;
     String nomBatiment;
-    Geocoder geocoder;
+
 
     Batiment(int i, String n){
         idBatiment=i;
         nomBatiment=n;
-        geocoder.getFromLocationName(nomBatiment,1);
-        Geocoder geocoder;
-        List<Address> addresses;
-        geocoder = new Geocoder(this, Locale.getDefault());
-
-        addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
-
-        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
-        String city = addresses.get(0).getLocality();
-        String state = addresses.get(0).getAdminArea();
-        String country = addresses.get(0).getCountryName();
-        String postalCode = addresses.get(0).getPostalCode();
-        String knownName = addresses.get(0).getFeatureName(); // Onl
-
     }
 
 
